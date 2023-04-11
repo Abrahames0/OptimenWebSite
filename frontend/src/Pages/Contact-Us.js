2d357cddf611c1bae5660a85151a9f785bcbc76e
ItzelFuentes <itzelfuentescabrera@gmail.com>
2023-04-11 10:47:39 -0600

    Modificación de nombres

--- frontend/src/Pages/Contact-Us.js
+++ frontend/src/Pages/Contact-Us.js
@@ -1,8 +1,8 @@
-//Autor: Espinoza Gomez Abraham Salvador
+//Autor: Espinoza Gómez Abraham Salvador
 
 import Nadvar from '../layouts/Nadvar'; // Importa el componente de la barra de navegación
 import Fotter from "../layouts/Footer"; // Importa el componente del pie de página
-import Contac from "../Components/contact-Us/Contact-Us"; // Importa el componente de la página de contacto
+import Contac from "../Components/Contact-Us/Contact-Us"; // Importa el componente de la página de contacto
 
 function ContactUs() {
   return (
